e4788f2678ee6c044c5b224f2f47468722919583
Abhishek Mishra <96767012+abhishekj7777@users.noreply.github.com>
2022-11-08 10:50:00 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,17 @@
+// Your First Program
+
+// Program to display numbers from 1 to 5
+
+class first{
+  public static void main(String[] args) {
+
+    // declare variables
+    int i = 1, n = 5;
+
+    // while loop from 1 to 5
+    while(i <= n) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
